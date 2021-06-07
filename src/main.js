773b1ac806bd0432889b5763b7faf2c96e3319b8
Chris Blassiaux <chrisblassiaux@gmail.com>
2021-06-07 11:41:58 +0200

    :sparkles: Fix #5 first http request initiated

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-import { createApp, Vue } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/main.css'
-import axios from 'axios'
-
-Vue.prototype.$http = axios;
 
 createApp(App).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,32 @@
     <div class="card">
       <h1 class="title">ABOUT THE UNIVERSE</h1>
       <p>Bienvenue sur ABOUT THE UNIVERSE pour découvrir toutes nos découvertes à propos du monde qui nous entoure !</p>
+      <router-link to="/about" class="btn mt-4" >Décourvrir</router-link>
+      <p>{{ user.data?.name }}</p>
+      <p>{{ user.data?.height }}</p>
+      <p>{{ user.data?.mass }}</p>
     </div>
   </main>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Home',
-  components: {}
+  data() {
+    return {
+      user: ''
+    }
+  },
+  methods: {
+
+  },
+  components: {},
+  mounted() {
+    axios.get('https://swapi.dev/api/people/3')
+        .then(response => this.user = response)
+        .catch(err => console.log(err))
+  }
 }
 </script>
