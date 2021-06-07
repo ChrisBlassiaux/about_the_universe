de53d0de8b89c70b91a3a5088653c83c522f4750
Chris Blassiaux <chrisblassiaux@gmail.com>
2021-06-07 10:53:56 +0200

    :sparkles: Fix #4 home-page component developed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,15 @@
 <template>
   <main class="container container">
     <div class="card">
-      <h1 class="title">HOME PAGE</h1>
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1 class="title">ABOUT THE UNIVERSE</h1>
+      <p>Bienvenue sur ABOUT THE UNIVERSE pour découvrir toutes nos découvertes à propos du monde qui nous entoure !</p>
     </div>
   </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
